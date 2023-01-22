a3148ea0634a8921bca99d929e2e15dcd86d0870
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2023-01-21 17:10:30 -0800

    Add LC 76 Solution

---
+++ 76-minimum-window-substring/76-minimum-window-substring.js
@@ -0,0 +1,62 @@
+// https://leetcode.com/problems/minimum-window-substring/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+
+/*Idea: We first try to match every occurrence of each character in the pattern with the string. If number of
+matches equals the pattern length, a window found. We try to shorten and remember the min length. If, while shortening
+it, the condition (matches equals the pattern length) fails, move to next potential window. 
+The shortening procedure will be repeated for the future windows*/
+
+/*Unlike permutation and anagram we can't rely on the logic: if (matches === map.size) return true;
+Why? Because here, the required substring can have some additional characters and
+doesn’t need to be a permutation of the pattern*/
+const minWindow = function (s, t) {
+  let matches = 0;
+  let substrStart = 0;
+  let minLength = s.length + 1;
+
+  //a hashmap to store the frequencies of all characters in the pattern
+  let map = new Map();
+
+  for (let i = 0; i < t.length; i++) {
+    map.set(t[i], (map.get(t[i]) || 0) + 1);
+  }
+  let windowStart = 0;
+  for (let windowEnd = 0; windowEnd < s.length; windowEnd++) {
+    //if the character being added matches a character in the HashMap,
+    //decrement its frequency
+    if (map.has(s[windowEnd])) {
+      map.set(s[windowEnd], map.get(s[windowEnd]) - 1);
+      ////count EVERY matching instance of a character
+      if (map.get(s[windowEnd]) >= 0) matches++;
+    }
+    //matched all characters => shrink the window to get the smallest window
+    while (matches === t.length) {
+      //minLength > current window length
+      if (minLength > windowEnd - windowStart + 1) {
+        //set it to window length
+        minLength = windowEnd - windowStart + 1;
+        substrStart = windowStart;
+      }
+      //character going out was in the pattern, then we must have decreased its frequency before
+      if (map.has(s[windowStart])) {
+        if (map.get(s[windowStart]) === 0) matches--;
+        //increment is frequency
+        map.set(s[windowStart], map.get(s[windowStart]) + 1);
+      }
+      windowStart++;
+    }
+  }
+  if (minLength > s.length) return '';
+  //return the substring
+  return s.substring(substrStart, substrStart + minLength);
+};
+
+minWindow('ADOBECODEBANC', 'ABC');
+
+//TC: O(n + m) where n and m are the number of characters in the input string and the pattern respectively.
+//SC: O(m) since in the worst case, the whole pattern can have distinct characters which will go into the hashmap. We also need O(n) space for the resulting substring, which will happen when the input string is a permutation of the pattern.
