182454050536c67a52e0b1fffa16239874dc27aa
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2023-04-18 22:45:43 -0700

    Add comments

--- 473-matchsticks-to-square/473-matchsticks-to-square.js
+++ 473-matchsticks-to-square/473-matchsticks-to-square.js
@@ -5,20 +5,24 @@
 
 const makesquare = function (matchsticks) {
   let sum = 0;
-  const sides = new Array(4).fill(0); //initialize sides as an arr = [0, 0, 0, 0]
+  //initialize sides as an arr = [0, 0, 0, 0] (currently empty; we need to build them up)
+  const sides = new Array(4).fill(0);
   for (let n of matchsticks) {
     sum += n;
   }
   //length of each side = total sum / 4
   const length = sum / 4;
   if (sum % 4 !== 0) return false; //9/4 = 2.25 => false (since matchstick lengths will just be integers)
+
+  //optimization: sort in desc order
+  //if length = 2 and longest/first stick = 3 => impossible to build square => false
   matchsticks.sort((a, b) => b - a);
 
   const backtrack = (i) => {
     if (i === matchsticks.length) return true;
     //for each matchstick, we have 4 choices (sides)
     for (let j = 0; j < 4; j++) {
-      //as long as the already existing matchstick + curr matchstick <= length of side
+      //as long as the curr side (made of prev sticks) + curr matchstick <= length of side
       if (sides[j] + matchsticks[i] <= length) {
         sides[j] += matchsticks[i]; //add it and explore further options
         if (backtrack(i + 1)) return true;
@@ -27,7 +31,7 @@ const makesquare = function (matchsticks) {
     }
     return false;
   };
-  return backtrack(0); //start from the 1st matchstick
+  return backtrack(0);
 };
 
 //TC: height of tree = n (size of input arr). At each leevl, we're making 4 choices => 4^n
