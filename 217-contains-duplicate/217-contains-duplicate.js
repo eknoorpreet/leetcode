5ac6228ac5b452524deb5423d0e01f2ac89861fa
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2023-10-08 22:40:51 -0700

    Revision for 217. Contains Duplicate

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -1,3 +1,21 @@
+/*Given an integer array nums, return true if any value appears at least twice in the array,
+and return false if every element is distinct.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+*/
+
 /**
  * @param {number[]} nums
  * @return {boolean}
@@ -5,14 +23,14 @@
 
 //TC: O(n^2)
 //SC: O(1)
-//  const containsDuplicate = function(nums) {
-//     for(let i = 0; i < nums.length - 1; i++) {
-//        for(let j = i + 1; j < nums.length; j++) {
-//            if(nums[i] === nums[j]) return true
-//        }
+// const containsDuplicate = function (nums) {
+//   for (let i = 0; i < nums.length - 1; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] === nums[j]) return true;
 //     }
-//     //loop ended => we never encountered a duplicate
-//     return false
+//   }
+//   //loop ended => we never encountered a duplicate
+//   return false;
 // };
 
 //TC: O(nlogn)
@@ -35,15 +53,11 @@
 const containsDuplicate = function (nums) {
   //go through all elements of array and add in set
   const set = new Set();
-  for (let i = 0; i < nums.length; i++) {
-    //if element not in set
-    if (!set.has(nums[i])) {
-      //add it
-      set.add(nums[i]);
-    } else {
-      //element already in set => duplicate encountered
-      return true;
-    }
+  for (let num of nums) {
+    //if element already in set => duplicate encountered
+    if (set.has(num)) return true;
+    //if not => add it
+    else set.add(num);
   }
   //loop ended => we never encountered a duplicate
   return false;
