e146947e305ab2d273440ec2cf8bce546f5afd73
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2023-10-25 22:33:17 -0700

    Add solution

---
+++ Arrays & Hashing/2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.js
@@ -0,0 +1,69 @@
+/*
+
+You are given a positive integer array nums.
+
+The element sum is the sum of all the elements in nums.
+The digit sum is the sum of all the digits (not necessarily distinct) that appear in nums.
+Return the absolute difference between the element sum and digit sum of nums.
+
+Note that the absolute difference between two integers x and y is defined as |x - y|.
+
+
+
+Example 1:
+
+Input: nums = [1,15,6,3]
+Output: 9
+Explanation:
+The element sum of nums is 1 + 15 + 6 + 3 = 25.
+The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
+The absolute difference between the element sum and digit sum is |25 - 16| = 9.
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: 0
+Explanation:
+The element sum of nums is 1 + 2 + 3 + 4 = 10.
+The digit sum of nums is 1 + 2 + 3 + 4 = 10.
+The absolute difference between the element sum and digit sum is |10 - 10| = 0.
+
+
+Constraints:
+
+1 <= nums.length <= 2000
+1 <= nums[i] <= 2000
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+const differenceOfSum0 = function (nums) {
+  let elementSum = 0;
+  let digitSum = 0;
+  for (let num of nums) {
+    elementSum += num;
+    for (let digit of `${num}`) {
+      digitSum += Number(digit);
+    }
+  }
+  return Math.abs(elementSum - digitSum);
+};
+
+const differenceOfSum = function (nums) {
+  let elementSum = 0;
+  let digitSum = 0;
+  for (let num of nums) {
+    elementSum += num;
+    while (num > 0) {
+      const digit = num % 10;
+      digitSum += digit;
+      num = Math.floor(num / 10);
+    }
+  }
+  return Math.abs(elementSum - digitSum);
+};
+//TC: O(n*k), n is the number of elements and k is the largest element
+//SC: O(1)
