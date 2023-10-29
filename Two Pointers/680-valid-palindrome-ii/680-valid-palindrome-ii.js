a99f4fa41ad302fd3fc26e5ecd6192969c8041be
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2023-10-29 16:05:52 -0700

    Organize files

--- Two Pointers/680-valid-palindrome-ii/680-valid-palindrome-ii.js
+++ Two Pointers/680-valid-palindrome-ii/680-valid-palindrome-ii.js
@@ -36,6 +36,17 @@ s consists of lowercase English letters.
  * @return {boolean}
  */
 
+/*
+
+Think of a palindrome: "race a car". Take any substring in a palindromic stirng and it'll be a
+palindrome. For example: "e a c", "ae a ca" and so on.
+
+Algorithm: Iterate over the string using two pointers to check if the string is a palindrome. At
+a point where s[left] and s[right] are not equal, it means the substring from [left, right] is not
+a palindrome. Now, check if excluding a character from either left or right side makes it a palindrome.
+If it does => true. If it doesn't => we used our "at most 1 character deletion" so => false
+
+*/
 const isPalindrome = function (s, left, right) {
   while (left <= right) {
     if (s.charAt(left) === s.charAt(right)) {
