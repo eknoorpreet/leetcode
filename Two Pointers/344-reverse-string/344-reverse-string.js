19b07b79b89d54110eb244fb6f4e3156f8af9802
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2023-10-28 17:47:25 -0700

    Added solution

---
+++ Two Pointers/344-reverse-string/344-reverse-string.js
@@ -0,0 +1,50 @@
+/*
+
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+
+Constraints:
+
+1 <= s.length <= 10^5
+s[i] is a printable ascii character.
+
+*/
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+
+const reverseString1 = function (s) {
+  return s.reverse();
+};
+
+const reverseString0 = function (s) {
+  let i = 0;
+  let j = s.length - 1;
+  while (i < j) {
+    //swap 0th and last chars and so on...
+    [s[i], s[j]] = [s[j], s[i]];
+    // let temp = s[i]
+    // s[i] = s[j]
+    // s[j] = temp
+    i++;
+    j--;
+  }
+  return s;
+};
+//TC: O(n)
+//SC: O(1)
