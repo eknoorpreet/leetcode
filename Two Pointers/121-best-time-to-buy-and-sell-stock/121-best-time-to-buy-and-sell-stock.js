13c0522d4d9a1d4db9689968d700203694cc75d5
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2023-11-17 00:10:46 -0800

    Add correct solution

--- Two Pointers/121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ Two Pointers/121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -1,37 +1,21 @@
 /*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
-On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
-
-Find and return the maximum profit you can achieve.
-
-
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
 Example 1:
 
 Input: prices = [7,1,5,3,6,4]
-Output: 7
-Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
-Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
-Total profit is 4 + 3 = 7.
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 Example 2:
-
-Input: prices = [1,2,3,4,5]
-Output: 4
-Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
-Total profit is 4.
-Example 3:
 
 Input: prices = [7,6,4,3,1]
 Output: 0
-Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
-
-
-Constraints:
-
-1 <= prices.length <= 3 * 10^4
-0 <= prices[i] <= 10^4
+Explanation: In this case, no transactions are done and the max profit = 0.
 
 */
 
@@ -41,17 +25,21 @@ Constraints:
  */
 
 const maxProfit = function (prices) {
-  let profit = 0;
-  for (let i = 1; i < prices.length; i++) {
-    //prices[i]: potential selling price
-    //prices[i - 1]: potential purchase price
-    //is there a profit
-    if (prices[i] > prices[i - 1]) {
-      //accumulate it (every time the value is in increasing order, add it)
-      profit += prices[i] - prices[i - 1];
+  //Two Pointers
+  let start = 0; // purchase price
+  let end = start + 1; // selling price
+  let maxProfit = 0;
+  while (start < end && end < prices.length) {
+    let profit = prices[end] - prices[start];
+    maxProfit = Math.max(maxProfit, profit);
+    // profit < 0 (purchase price is higher than curr price)
+    // => make curr price the potential instead!
+    if (profit < 0) {
+      start = end;
     }
+    end++;
   }
-  return profit;
+  return maxProfit;
 };
 
 //TC: O(n)
