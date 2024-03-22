dfdf1e7c0264b03f78be77dd15a27b45af1e5e62
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-03-21 23:01:04 -0700

    Completed 61. Rotate List

---
+++ Linked Lists/61-rotate-list/61-rotate-list.js
@@ -0,0 +1,67 @@
+/*
+
+Given the head of a linked list, rotate the list to the right by k places.
+
+
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+Example 2:
+
+
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 500].
+-100 <= Node.val <= 100
+0 <= k <= 2 * 10^9
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+
+const rotateRight = function (head, k) {
+  if (!head || !head.next) return head;
+  let headCopy = head;
+  let size = 0;
+  while (headCopy) {
+    headCopy = headCopy.next;
+    size++;
+  }
+  k = k % size;
+  if (k === 0) return head;
+
+  let currentNode = head;
+  let prev = null;
+  while (k > 0) {
+    while (currentNode && currentNode.next) {
+      prev = currentNode;
+      currentNode = currentNode.next;
+    }
+    prev.next = null;
+    currentNode.next = head;
+    head = currentNode;
+    k--;
+  }
+  return head;
+};
+
+// TC: O(n)
+// SC: O(1)
