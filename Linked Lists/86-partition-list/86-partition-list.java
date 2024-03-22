858907d6c46ced022b1fba584871734188002782
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-03-22 03:50:50 -0700

    Completed 86. Partition List (Java)

---
+++ Linked Lists/86-partition-list/86-partition-list.java
@@ -0,0 +1,61 @@
+/*
+
+Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+
+
+Example 1:
+
+
+Input: head = [1,4,3,2,5,2], x = 3
+Output: [1,2,2,4,3,5]
+Example 2:
+
+Input: head = [2,1], x = 2
+Output: [1,2]
+
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 200].
+-100 <= Node.val <= 100
+-200 <= x <= 200
+
+*/
+
+class ListNode {
+  int val;
+  ListNode next;
+  ListNode() {}
+  ListNode(int val) { this.val = val; }
+  ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+  public ListNode partition(ListNode head, int x) {
+      ListNode list1 = new ListNode(0, head);
+      ListNode p1 = list1;
+      ListNode list2 = new ListNode(0, head);
+      ListNode p2 = list2;
+      ListNode currentNode = head;
+
+      while(currentNode != null) {
+          if(currentNode.val < x) {
+              p1.next = currentNode;
+              p1 = p1.next;
+          } else {
+              p2.next = currentNode;
+              p2 = p2.next;
+          }
+          currentNode = currentNode.next;
+      }
+      p2.next = null;
+      p1.next = list2.next;
+      return list1.next;
+  }
+}
+
+// TC: O(n)
+// SC: O(1)
