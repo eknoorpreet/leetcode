17f5830bb83be74bd02384eaae8c359a2631ae3c
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-04-28 11:14:44 -0700

    Completed 622. Design Circular Queue

---
+++ Linked Lists/622-design-circular-queue/622-design-circular-queue.js
@@ -0,0 +1,99 @@
+/*
+
+Design your implementation of the circular queue. The circular queue is a linear data structure in which the operations are performed based on FIFO (First In First Out) principle, and the last position is connected back to the first position to make a circle. It is also called "Ring Buffer".
+
+One of the benefits of the circular queue is that we can make use of the spaces in front of the queue. In a normal queue, once the queue becomes full, we cannot insert the next element even if there is a space in front of the queue. But using the circular queue, we can use the space to store new values.
+
+Implement the MyCircularQueue class:
+
+MyCircularQueue(k) Initializes the object with the size of the queue to be k.
+int Front() Gets the front item from the queue. If the queue is empty, return -1.
+int Rear() Gets the last item from the queue. If the queue is empty, return -1.
+boolean enQueue(int value) Inserts an element into the circular queue. Return true if the operation is successful.
+boolean deQueue() Deletes an element from the circular queue. Return true if the operation is successful.
+boolean isEmpty() Checks whether the circular queue is empty or not.
+boolean isFull() Checks whether the circular queue is full or not.
+You must solve the problem without using the built-in queue data structure in your programming language.
+
+*/
+
+/**
+ * @param {number} k
+ */
+function Node(val, next, prev) {
+  this.val = val;
+  this.prev = prev;
+  this.next = next;
+}
+
+const MyCircularQueue = function (k) {
+  this.head = new Node(0, null, null);
+  this.tail = new Node(0, null, this.head);
+  this.head.next = this.tail;
+  this.capacity = k;
+};
+
+/**
+ * @param {number} value
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.enQueue = function (value) {
+  if (this.isFull()) return false;
+  let newNode = new Node(value, this.tail, this.tail.prev);
+  this.tail.prev.next = newNode;
+  this.tail.prev = newNode;
+  this.capacity--;
+  return true;
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.deQueue = function () {
+  if (this.isEmpty()) return false;
+  this.head.next = this.head.next.next;
+  this.head.next.prev = this.head;
+  this.capacity++;
+  return true;
+};
+
+/**
+ * @return {number}
+ */
+MyCircularQueue.prototype.Front = function () {
+  if (this.isEmpty()) return -1;
+  return this.head.next.val;
+};
+
+/**
+ * @return {number}
+ */
+MyCircularQueue.prototype.Rear = function () {
+  if (this.isEmpty()) return -1;
+  return this.tail.prev.val;
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isEmpty = function () {
+  return this.head.next === this.tail;
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isFull = function () {
+  return this.capacity === 0;
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * var obj = new MyCircularQueue(k)
+ * var param_1 = obj.enQueue(value)
+ * var param_2 = obj.deQueue()
+ * var param_3 = obj.Front()
+ * var param_4 = obj.Rear()
+ * var param_5 = obj.isEmpty()
+ * var param_6 = obj.isFull()
+ */
