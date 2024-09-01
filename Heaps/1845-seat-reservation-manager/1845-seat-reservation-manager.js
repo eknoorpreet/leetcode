f4ecbbf77708a6cb3da20aca7b4952262a0a9474
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-09-01 11:10:25 +0530

    Completed 1845. Seat Reservation Manager

---
+++ Heaps/1845-seat-reservation-manager/1845-seat-reservation-manager.js
@@ -0,0 +1,175 @@
+/*
+
+Design a system that manages the reservation state of n seats that are numbered from 1 to n.
+
+Implement the SeatManager class:
+
+SeatManager(int n) Initializes a SeatManager object that will manage n seats numbered from 1 to n. All seats are initially available.
+int reserve() Fetches the smallest-numbered unreserved seat, reserves it, and returns its number.
+void unreserve(int seatNumber) Unreserves the seat with the given seatNumber.
+
+
+Example 1:
+
+Input
+["SeatManager", "reserve", "reserve", "unreserve", "reserve", "reserve", "reserve", "reserve", "unreserve"]
+[[5], [], [], [2], [], [], [], [], [5]]
+Output
+[null, 1, 2, null, 2, 3, 4, 5, null]
+
+Explanation
+SeatManager seatManager = new SeatManager(5); // Initializes a SeatManager with 5 seats.
+seatManager.reserve();    // All seats are available, so return the lowest numbered seat, which is 1.
+seatManager.reserve();    // The available seats are [2,3,4,5], so return the lowest of them, which is 2.
+seatManager.unreserve(2); // Unreserve seat 2, so now the available seats are [2,3,4,5].
+seatManager.reserve();    // The available seats are [2,3,4,5], so return the lowest of them, which is 2.
+seatManager.reserve();    // The available seats are [3,4,5], so return the lowest of them, which is 3.
+seatManager.reserve();    // The available seats are [4,5], so return the lowest of them, which is 4.
+seatManager.reserve();    // The only available seat is seat 5, so return 5.
+seatManager.unreserve(5); // Unreserve seat 5, so now the available seats are [5].
+
+
+Constraints:
+
+1 <= n <= 10^5
+1 <= seatNumber <= n
+For each call to reserve, it is guaranteed that there will be at least one unreserved seat.
+For each call to unreserve, it is guaranteed that seatNumber will be reserved.
+At most 10^5 calls in total will be made to reserve and unreserve.
+
+*/
+
+/**
+ * @param {number} n
+ */
+
+/**
+ * @param {string[]} nums
+ * @param {number} k
+ * @return {string}
+ */
+
+class MinHeap {
+  constructor() {
+    this.values = []; //an array that will hold heap data
+  }
+
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp();
+    return this;
+  }
+
+  bubbleUp() {
+    let currentIdx = this.values.length - 1;
+    const element = this.values[currentIdx];
+
+    while (currentIdx > 0) {
+      let parentIdx = Math.floor((currentIdx - 1) / 2);
+      let parent = this.values[parentIdx];
+      //in min heap, parent should be lesser than element
+      //as long as the opposite is the case => keep swapping
+      if (element < parent) {
+        this.values[parentIdx] = element;
+        this.values[currentIdx] = parent;
+        // [this.values[parentIdx], this.values[currentIdx]] = [element, parent]
+        currentIdx = parentIdx;
+      } else {
+        break;
+      }
+    }
+    return this;
+  }
+
+  extractMin() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        //make sure it's in bounds
+        leftChild = this.values[leftChildIdx];
+        if (leftChild < element) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        //make sure it's in bounds
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild < element) ||
+          (swap !== null && rightChild < leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+
+  peek() {
+    return this.values[0];
+  }
+}
+
+/*
+
+  reserve() and unreserve() calls will always be valid. If all seats are reserved, reserve()
+  won't be called. If all seats are unreserved, unreserve() won't be called.
+  Therefore, no need for 2 heaps to keep track of both. Just 1 heap for unreserved is enough
+  */
+
+const SeatManager = function (n) {
+  // smallest-numbered unreserved seat
+  this.unreserved = new MinHeap();
+  // in the beginning, all seats are unreserved
+  for (let i = 1; i <= n; i++) {
+    // Add to the 'unreserve' collection
+    this.unreserved.insert(i);
+  }
+};
+
+/**
+ * @return {number}
+ */
+SeatManager.prototype.reserve = function () {
+  // Return the smallest-numbered unreserved seat
+  return this.unreserved.extractMin();
+};
+
+/**
+ * @param {number} seatNumber
+ * @return {void}
+ */
+SeatManager.prototype.unreserve = function (seatNumber) {
+  // Unreserve the seat (remove the 'unreserve' collection)
+  this.unreserved.insert(seatNumber);
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * var obj = new SeatManager(n)
+ * var param_1 = obj.reserve()
+ * obj.unreserve(seatNumber)
+ */
