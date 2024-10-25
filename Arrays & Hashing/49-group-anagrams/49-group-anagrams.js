31a45a861728f38c22520c621c9e2b1224f394e3
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-10-25 11:40:39 -0700

    49. Group Anagrams: Updated explanation

--- Arrays & Hashing/49-group-anagrams/49-group-anagrams.js
+++ Arrays & Hashing/49-group-anagrams/49-group-anagrams.js
@@ -18,14 +18,17 @@ SC: At worst, the number of keys and groups in the hashmap is the same as the nu
 https://leetcode.com/problems/group-anagrams/solutions/718955/three-js-solutions/comments/1084554
 */
 
-//Sorting takes a significant time. How about we "key" the anagrams some other way?
+// The most expensive operation was the generation of keys (sorting).
+// How about we "key" the anagrams some other way?
 const groupAnagrams = function (strs) {
-  //we will build the result as a hashmap of [key] => [anagrams]
+  // We will build the result as a hashmap of [key] => [anagrams] just like before
   const res = {};
-  //go throough all strings
+  // Go through all strings
   for (let str of strs) {
-    //for each string, a new 26-length (indexed 0 - 25) [] with 0s
-    let count = new Array(26).fill(0);
+    // For each string, a new 26-length (indexed 0 - 25) [] with 0s
+    // This will have frequencies of characters for that word
+    // We will use this array to generate they key
+    const count = new Array(26).fill(0);
     //'a'.charCodeAt()-97 => 0 (0th index will have counts for 'a')
     //'z'.charCodeAt()-97 => 25 (25th index will have counts for 'z')
     for (let char of str) {
@@ -33,15 +36,15 @@ const groupAnagrams = function (strs) {
       //'e'.charCodeAt() - 97 => 4
       //count[4] = 1
 
-      //'a'.charCodeAt() - 97 => 1
-      //count[1] = 1
+      //'a'.charCodeAt() - 97 => 0
+      //count[0] = 1
 
       //'t'.charCodeAt() - 97 => 19
       //count[19] = 1
 
-      //store frequencies of curr char in 'count' via .charCodeAt()
-      const code = char.charCodeAt() - 97;
-      count[code]++;
+      // Store frequencies of curr char in 'count' via .charCodeAt()
+      const index = char.charCodeAt() - 97;
+      count[index]++;
     }
     /*.join(""): It will fail in certain scenarios where the input has 10
       characters repeated.
@@ -50,27 +53,15 @@ const groupAnagrams = function (strs) {
          have hash key value as 010100000000000000000000000
       Note, that first string has 10 d's in it & second string has 10 b's in it.*/
 
-    //build a key for curr group
-    //O(26) operation, essentially O(1)
-    const key = count.join('#');
+    // Build a key for curr group
+    // O(26) operation, essentially O(1)
+    let key = count.join('#');
     //a group of anagrams under 1 key
 
     // if(!result[key]) {
     //     result[key] = []
     // }
     // result[key].push(str)
-
-    //all strings resulting intp the same key (same char with same freq) will be under that 1 key
-    // console.log(res)
-    /*
-
-{
-'1#0#0#0#1#0#0#0#0#0#0#0#0#0#0#0#0#0#0#1#0#0#0#0#0#0': [ 'eat', 'tea', 'ate' ],
-'1#0#0#0#0#0#0#0#0#0#0#0#0#1#0#0#0#0#0#1#0#0#0#0#0#0': [ 'tan', 'nat' ],
-'1#1#0#0#0#0#0#0#0#0#0#0#0#0#0#0#0#0#0#1#0#0#0#0#0#0': [ 'bat' ]
- }
-
- */
     res[key] ? res[key].push(str) : (res[key] = [str]);
   }
   return Object.values(res);
