cd8cff4d911584440107a87e9b3113ecb8299240
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-11-08 21:13:01 -0800

    Updated explanation: 189. Rotate Array

--- Two Pointers/189-rotate-array/189-rotate-array.js
+++ Two Pointers/189-rotate-array/189-rotate-array.js
@@ -32,33 +32,35 @@ Constraints:
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 
-//rotate => shifting each element to right by k times (last ones will be looped back to front)
-//pattern: How to cycle back in array? Each element => (i + k) % nums.length => always stays in array!
-//i(4) + k(2) = 6 % 5 = index 1
+// rotate => shifting each element to right by k times (last ones will be looped back to front)
+// Pattern: How to cycle back in array? Each element => (i + k) % nums.length => always stays in array!
+// i(4) + k(2) = 6 % 5 = index 1
 const rotate = function (nums, k) {
-  //k could be greater than array length
-  //k = array.length => k % length = rotate 0 times (just return)
-  if (k % nums.length === 0) return nums;
-  //k > array.length => k (5) % length (4) = rotate 1 time
-
-  //what if we have k = 101 and array length is 7, then we will not rotate it 101 times. It
-  //simply means we will rotate only 100 % = 2 times => [6,7,1,2,3,4,5]
+  // k could be greater than array length
+  // What if we have k = 100 and array length is 7, then we will not rotate it 100 times.
+  // It simply means we will rotate only 100 % = 2 times => [6,7,1,2,3,4,5]
+  // k > array.length => k (5) % length (4) = rotate 1 time
 
   k = k % nums.length;
 
-  /*[1,2,3,4,5,6,7], k = -1
+  // k = array.length => k % length = rotate 0 times (just return)
+  if (k % nums.length === 0) return nums;
+
+  /*
+  [1,2,3,4,5,6,7], k = -1
   Output : [2,3,4,5,6,7,1]
 
-  k = -1 => k = 6. So, what It represent is that add the -ve value to the length of array.*/
+  k = -1 => k = 6.
+  So, what it represents is that add the -ve value to the length of array.*/
 
   if (k < 0) {
     // if we get -ve value, then -ve is just equals to it's -ve + array.length
     k += nums.length;
   }
 
-  //input arr = [1,2,3,4,5,6,7], k = 3, result should be: [5,6,7,1,2,3,4]
-  //input arr into 2 portions:
-  //part 1: [1,2,3,4], part 2: [5,6,7]
+  // input arr = [1,2,3,4,5,6,7], k = 3, result should be: [5,6,7,1,2,3,4]
+  // input arr into 2 portions:
+  // part 1: [1,2,3,4], part 2: [5,6,7]
 
   reverse(nums, 0, nums.length - 1); //entire arr reversed => [1,2,3,4,5,6,7] => [7,6,5,4,3,2,1]
   reverse(nums, 0, k - 1); // k elements (now at front) [7,6,5] => [5,6,7] => [5,6,7,4,3,2,1]
