feea0b9ecd2c6c3224e9c94c2374a65ef45148ee
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-11-09 14:20:54 -0800

    Cleaned code: 155. Min Stack

--- Stack/155-min-stack/155-min-stack.js
+++ Stack/155-min-stack/155-min-stack.js
@@ -70,11 +70,11 @@ class Stack {
 }
 
 const MinStack = function () {
-  //2 stacks (push/pop from both stacks)
-  //1st stack
-  this.stack = new Stack(); //responsibe for getting the top value
-  //2nd stack
-  this.minStack = new Stack(); //responsibe for getting the min value (getmin())
+  // 2 stacks (push/pop from both stacks)
+  // 1st stack: responsibe for getting the top value
+  this.stack = new Stack();
+  // 2nd stack: responsibe for getting the min value (getmin())
+  this.minStack = new Stack();
 };
 
 /**
@@ -82,42 +82,35 @@ const MinStack = function () {
  * @return {void}
  */
 MinStack.prototype.push = function (val) {
-  //push val to 1st stack normally
+  // Push val to 1st stack normally
   this.stack.push(val);
-  //compare incoming val to current min
-  let newVal = this.minStack.length
+  // Compare incoming val to current min
+  let newMinVal = this.minStack.length
     ? Math.min(val, this.minStack.peek().value)
     : val;
   //push min value to minStack (on top)
-  this.minStack.push(newVal);
+  this.minStack.push(newMinVal);
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function () {
-  //pop from both stacks
+  // Pop from both stacks
 
-  //pushed: -2, 0, -3
-  //stack = [-2], minStack = [-2]
-  //stack = [-2, 0], minStack = [-2, -2]
-  //stack = [-2, 0, -3], minStack = [-2, -2, -3]
+  // Pushed: -2, 0, -3
+  // stack = [-2], minStack = [-2]
+  // stack = [-2, 0], minStack = [-2, 0]
+  // stack = [-2, 0, -3], minStack = [-2, -2, -3]
 
-  //pop()
-  //stack = [-2, 0], minStack = [-2, -2]
-  //when we pop top of stack from stack, we also pop the corresponding minimum
-  //from minStack (the value that was min at that point)
-  //now, stack's top = 0, minStack's top = -2
-
-  // what if our stacks are:
-  // stack: [-2, 0, -3, -1]
-  // minStack: [-2, -2, -3, -3]
-  // We pop different values (-1 and -3) from our stacks but that's fine since
-  // minStack doesn't care about -1, just the min values. And the second last value is also
-  // -3
-  this.stack.pop(); //will remove latest-added value
-  this.minStack.pop(); //will remove min value
-  //and now the top of min stack will have the new min value
+  // pop()
+  // stack = [-2, 0], minStack = [-2, -2]
+  // When we pop top of stack from stack, we also pop the corresponding minimum
+  // from minStack (the value that was min at that point)
+  // Now, stack's top = 0, minStack's top = -2
+  this.stack.pop(); // Will remove latest-added value
+  this.minStack.pop(); // Will remove min value
+  // And now the top of min stack will have the new min value
 };
 
 /**
@@ -144,5 +137,5 @@ MinStack.prototype.getMin = function () {
  * var param_4 = obj.getMin()
  */
 
-//TC: O(n)
-//SC: O(n)
+// TC: O(1)
+// SC: O(n)
