ec4a87bfc47ce9f2b16eff257c6df04593dc1637
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-11-10 22:47:27 -0800

    Updated explanation: 144. Binary Tree Preorder Traversal

--- Trees/144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
+++ Trees/144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
@@ -60,46 +60,57 @@ class Stack {
   }
 }
 
-// const preorderTraversal = function(root) {
-//     const result = []
-//     if(!root) return sorted;
-//     const traverse = (node) => {
-//         result.push(node.val)
-//         if(node.left) traverse(node.left)
-//         if(node.right) traverse(node.right)
-//         return result
-//     }
-//     return traverse(root)
-// };
+/*
+
+In preorder traversal, we visit nodes in this order: ROOT -> LEFT -> RIGHT
+For example, in the tree [1,null,2,3]:
+
+     1
+      \
+       2
+      /
+     3
+
+We visit 1 first (root), then traverse to the right since there's no left child (getting 2), then to 2's left child (getting 3).
+
+         4
+       /   \
+      2     6
+     / \   / \
+    1   3 5   7
+
+Preorder: [4,2,1,3,6,5,7]
+
+*/
 
 /*
-           4
-         /   \
-        2     6
-       / \   / \
-      1   3 5   7
-  
-  Preorder: [4,2,1,3,6,5,7]
-  
-  */
 
-const preorderTraversal = function (root) {
+Iterative:
+
+The Stack's Purpose:
+
+The stack helps us keep track of nodes we need to revisit later (Remember where we came from (our path))
+Backtrack when we reach a null node
+When we go left, we store the parent node so we can come back to check its right subtree. Basically,
+it helps us keep track of nodes whose right subtrees we still need to explore
+
+*/
+
+const preorderTraversal0 = function (root) {
   const result = [];
   if (!root) return result;
-  const stack = new Stack();
-  let currentNode = root;
-  while (currentNode || stack.length) {
-    while (currentNode) {
-      //the node being processed first
-      result.push(currentNode.val);
-      stack.push(currentNode);
-      currentNode = currentNode.left;
-    }
-    currentNode = stack.pop();
-    currentNode = currentNode.right;
-  }
-  return result;
+  const traverse = (node) => {
+    result.push(node.val);
+    if (node.left) traverse(node.left);
+    if (node.right) traverse(node.right);
+    return result;
+  };
+  return traverse(root);
 };
 
-//TC: O(n)
-//SC: O(n)
+/*
+
+Time complexity: O(n) where n is the number of nodes
+Space complexity: O(h) where h is the height of the tree (maximum stack size)
+
+*/
