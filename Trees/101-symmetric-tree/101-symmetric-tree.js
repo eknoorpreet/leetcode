b5fe55cc32e2d7d6146b08f29b0c83680500c4e7
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-11-11 01:35:51 -0800

    Updated explanation: 101. Symmetric Tree

--- Trees/101-symmetric-tree/101-symmetric-tree.js
+++ Trees/101-symmetric-tree/101-symmetric-tree.js
@@ -1,3 +1,28 @@
+/*
+
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+
+
+Example 1:
+
+
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+Example 2:
+
+
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+-100 <= Node.val <= 100
+
+*/
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -11,20 +36,25 @@
  * @return {boolean}
  */
 
-// a helper function to check if 2 trees are the same (here, we check if left and right subtrees are the same)
-const dfs = function (p, q) {
-  //if both are null => same tree!
+// A helper function to check if 2 trees are the same (here, we check if left and right subtrees are the same)
+const isSameTree = function (p, q) {
+  // If both are null => same tree!
   if (!p && !q) return true;
-  //if (strictly) only 1 exists (situation of both not existing would have been caught above) => not same tree
+  // If (strictly) only 1 exists (situation of both not existing would have been caught above) => not
+  // same tree
   if (!p || !q) return false;
-  //check current node's value of both trees and recursively check their left and right subtrees
-  return p.val === q.val && dfs(p.left, q.right) && dfs(p.right, q.left);
+  // Check current node's value of both trees and recursively check their left and right subtrees
+  return (
+    p.val === q.val &&
+    isSameTree(p.left, q.right) &&
+    isSameTree(p.right, q.left)
+  );
 };
 
 const isSymmetric = function (root) {
   if (!root) return true;
-  //for a tree to be symmetrical, its 2 subtrees need to be symmetrical
-  return dfs(root.left, root.right);
+  // For a tree to be symmetrical, its 2 subtrees need to be symmetrical
+  return isSameTree(root.left, root.right);
 };
 
 //TC: O(n)
