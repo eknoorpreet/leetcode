a909c85e1f33b7abf46d84c9748c0bfa1e2fff89
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-11-11 23:38:29 -0800

    Updated explanation: 236. Lowest Common Ancestor of a Binary Tree

--- Trees/236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
+++ Trees/236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
@@ -12,6 +12,20 @@
  * @return {TreeNode}
  */
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+
 /*
           3
        /     \
@@ -22,41 +36,47 @@
       7   4
 
 Say p = 5, q = 1
-If both left and right children returned a node (5 and 1), means both p and q found so parent (curr node) is LCA
-Otherwise, if we only found p (5) and no q in the right subtree, that must mean it will be found under p => p is ancestor of q. And vice-versa.
+
+LCA:
+    1. We can reach/find both p and q via the LCA node
+    2. It should be at the deepest level (first one since recursion goes bottom-top)
+
+3 cases:
+    1. Both p and q are on opposite sides
+    2. q is a descendant of p (p is the ancestor of q)
+    3. p is a descendant of q (q is the ancestor of p)
+
+We can look for p and q in both the left and right sub-trees. If both left and right sub-trees returned the values of p and q (indicating, we found them), therefore, parent (curr node) is LCA
+
+Otherwise, if we only found p (5) in the left subtree and no q in the right subtree, that must mean it will be found under p => p is ancestor of q. And vice-versa.
+
+Why don't we just return true when we find p or q? Well, that would only work in the 1st case when both p and q are on opposite sides. It means, that if current node is 3, p = 5, q = 1, both return true => return the current node => Correct! But, if p = 5, q = 4 (q is a descendant of p), then the left subtree will return true on finding p but the right subtree will return false as it could not find p or q (since both are in the left subtree). Therefore, left || right => true || false will return true (indicating that we found one of them) but we need to return the actual value of the node we found as that's the LCA.
+
 */
 
 const lowestCommonAncestor = function (root, p, q) {
-  //recursion: always focus on a single node
-  /*LCA:
-      1. We can reach/find both p and q via the LCA node
-      2. it should be at the deepest level (first one since recursion goes bottom-top)
+  // Does the curr node equal to p or q? If it does, then either the other one is on the other side of
+  // the tree or it's a descendant of the first
 
-  3 cases:
-      1. Both p and q are on opposite sides
-      2. q is a descendant of p
-      3. p is a descendant of q
-  Does the curr node equal to p or q? If it does, then either the other one is on the other side of the tree or it's a descendant of the first
-  receive a non-null node from both side => LCA*/
-
-  //we try to find both p or q
-  //either it's the curr node (or its at deeper levels)
-  if (!root || root === p || root === q) return root;
-  let left;
-  let right;
-  //else, look in its left and right subtrees
+  // We try to find both p or q
+  // Either it's the curr node (or its at deeper levels)
+  if (root === p || root === q) return root;
+  let left = null;
+  let right = null;
+  // else, look in its left and right subtrees
   if (root.left) {
     left = lowestCommonAncestor(root.left, p, q);
   }
   if (root.right) {
     right = lowestCommonAncestor(root.right, p, q);
   }
-  //what was the base case? curr node equal to p or q
-  //therefore, if both left and right children returned a node above to a specific node (which they return only if they're equal to p and q), means
+  // What was the base case? curr node equal to p or q
+  // therefore, if both left and right children returned a node above to a specific node (which they return only if they're equal to p and q), means
   //both p and q found so that specific node (parent) is LCA
+
   if (left && right) return root;
-  //if we found only or only q => that is the LCA (since it's given that both exist)
-  //(if they're not equal to p or q, they're implicitly returning null)
+  // If we found only p or only q => that is the LCA (since it's given that both exist)
+  // (if they're not equal to p or q, they're implicitly returning null)
   else return left || right; //return left ?? right
 };
 
