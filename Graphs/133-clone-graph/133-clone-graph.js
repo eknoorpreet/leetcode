632a5d11e516c6388f0379c3424501e07a4c591f
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-11-13 04:21:09 -0800

    Updated explanation: 133. Clone Graph

--- Graphs/133-clone-graph/133-clone-graph.js
+++ Graphs/133-clone-graph/133-clone-graph.js
@@ -65,13 +65,19 @@ The DFS traversal has a time complexity of O(V + E). This is because it visits e
 Space Complexity:
 
 The space complexity is determined by the additional data structures used during the DFS traversal:
-oldToNew Map: The oldToNew map stores mappings from original nodes to their corresponding cloned nodes. In the worst case, it can contain entries for all nodes in the graph. Therefore, the space complexity due to the map is O(V).
+originalToCopy Map: The originalToCopy map stores mappings from original nodes to their
+corresponding cloned nodes. In the worst case, it can contain entries for all nodes in the graph.
+Therefore, the space complexity due to the map is O(V).
 
-DFS Call Stack: The DFS traversal is implemented recursively, which means there is a function call stack. In the worst case, the DFS stack depth can be as large as the number of nodes in the graph (O(V)).
+DFS Call Stack: The DFS traversal is implemented recursively, which means there is a function call
+stack. In the worst case, the DFS stack depth can be as large as the number of nodes in the
+graph (O(V)).
 
-Node Copies: The cloned nodes are stored in memory as the copy is made. In the worst case, there can be a copy for each node in the graph, which is O(V).
+Node Copies: The cloned nodes are stored in memory as the copy is made. In the worst case,
+there can be a copy for each node in the graph, which is O(V).
 
 Combining these space complexities, the overall space complexity is O(V + V) = O(2V), which simplifies to O(V).
-In summary, the time complexity of the code is O(V + E), and the space complexity is O(V). These complexities are based on the worst-case scenario where all nodes and edges are visited and cloned.
+In summary, the time complexity of the code is O(V + E), and the space complexity is O(V).
+These complexities are based on the worst-case scenario where all nodes and edges are visited and cloned.
 
 */
