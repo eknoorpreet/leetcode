6a28cfdf1a0d21caf774810184fe3a856bd3f1d5
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-11-20 23:30:54 -0800

    Completed 485. Max Consecutive Ones

---
+++ Arrays & Hashing/485-max-consecutive-ones/485-max-consecutive-ones.js
@@ -0,0 +1,45 @@
+/*
+
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+
+
+Example 1:
+
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+Example 2:
+
+Input: nums = [1,0,1,1,0,1]
+Output: 2
+
+
+Constraints:
+
+1 <= nums.length <= 10^5
+nums[i] is either 0 or 1.
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findMaxConsecutiveOnes = function (nums) {
+  let currCount = 0;
+  let maxCount = 0;
+  for (let num of nums) {
+    if (num === 1) {
+      currCount++;
+    } else {
+      maxCount = Math.max(maxCount, currCount);
+      currCount = 0;
+    }
+  }
+  // What if the last number is 1? The maxCount will not be updated. Take that into account
+  return currCount > maxCount ? currCount : maxCount;
+};
+
+// TC: O(n)
+// SC: O(1)
